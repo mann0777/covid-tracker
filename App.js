eca0af4625a66f47cb2e6a7804f62560a176dd01
mann0777 <mannamloh77@gmail.com>
2022-05-27 11:25:06 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import Covid from "./components/covid.js"
+import Covid from "./components/covid.css"
+
+const App =()=> {
+return(
+  <>
+    <Covid />
+  </>
+  
+)
+
+}
+
+export default App
